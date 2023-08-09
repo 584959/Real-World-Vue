d3a7c10e25b6885553fd0f21eb27b9903f82b958
Bernt Munchow <bernt.munchow@tryg.no>
2023-08-09 08:59:20 +0200

    initial commit, ready for deploy

---
+++ src/views/EventListView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import EventCard from '../components/EventCard.vue';
+import EveentService from '../services/EventService'
+
+const events = ref<Event[]>([])
+
+interface Event  {
+  id: string,
+  category: string,
+  title: string,
+  description: string,
+  location: string,
+  date: Date,
+  time: Date,
+  organizer: string
+}
+
+onMounted(() => {
+  EveentService.getEvents()
+  .then((response) => {
+    events.value = response.data;
+  })
+  .catch((error) => {
+    console.log(error);
+    
+  })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" :eventId="event.id" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
